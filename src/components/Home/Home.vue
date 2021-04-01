046f22690af7fb9b599456da58e5e2d22c66a208
sallythetechie <sllmusanga7@gmail.com>
2021-04-01 06:34:31 +0300

    add bootstrap-vue modals for adding and updating events

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <div class="sidenav">
-      <a href="#about">About</a>
-      <a href="#services">Services</a>
-      <a href="#clients">Clients</a>
-      <a href="#contact">Contact</a>
+    <div class="sidenav text-left">
+      <h2 class="ml-2 text-white text-center">Event Finder</h2>
+      <div class="links">
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+      </div>
     </div>
     <div class="main">
-      <h1 class="text-left ">Events/Dashboard</h1>
-      <hr>
+      <h1 class="text-left">Dashboard</h1>
+      <hr />
+      <alert :message=message v-if="showMessage"></alert>
       <div class="centered">
         <div class="row">
-          <div class=" col-12">
+          <div class="col-12">
             <div class="div-outline">
               <div class="row center">
                 <h3 class="">Events</h3>
-                <button type="button" class="btn btn-rounded btn-sm">
+                <button
+                  type="button"
+                  class="btn btn-rounded btn-sm"
+                  v-b-modal.event-modal
+                >
+                  <b-icon icon="plus"></b-icon>
                   Add Event
                 </button>
               </div>
@@ -40,10 +49,12 @@
                   </td>
                   <td>
                     <div class="btn-group" role="group">
-                      <button type="button" class="btn btn-warning btn-sm">
+                      <button type="button" v-b-modal.event-update-modal
+        @click="editEvent(event)"  class="btn btn-warning btn-sm">
                         Update
                       </button>
-                      <button type="button" class="btn btn-danger btn-sm">
+                      <button type="button" class="btn btn-danger btn-sm"
+                       @click="onDeleteEvent(event)">
                         Delete
                       </button>
                     </div>
@@ -55,17 +66,169 @@
         </div>
       </div>
     </div>
+    <b-modal
+      ref="addEventModal"
+      id="event-modal"
+      title="Add a new event"
+      hide-footer
+    >
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+        <b-form-group
+          id="form-name-group"
+          label="Name:"
+          label-for="form-name-input"
+        >
+          <b-form-input
+            id="form-name-input"
+            type="text"
+            v-model="addEventForm.name"
+            required
+            placeholder="Enter name"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="form-location-group"
+          label="Location:"
+          label-for="form-location-input"
+        >
+          <b-form-input
+            id="form-location-input"
+            type="text"
+            v-model="addEventForm.location"
+            required
+            placeholder="Enter location"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="form-date-group"
+          label="Date:"
+          label-for="form-date-input"
+        >
+          <b-form-input
+            id="form-date-input"
+            type="text"
+            v-model="addEventForm.date"
+            required
+            placeholder="Enter date"
+          >
+          </b-form-input>
+        </b-form-group>
+          <b-form-group
+            id="form-time-group"
+            label="Time:"
+            label-for="form-time-input"
+          >
+            <b-form-input
+              id="form-time-input"
+              type="text"
+              v-model="addEventForm.time"
+              required
+              placeholder="Enter time"
+            >
+            </b-form-input>
+        </b-form-group>
+        <b-button type="submit" class="mr-2" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
+    <b-modal
+      ref="editEventModal"
+      id="event-update-modal"
+      title="Update"
+      hide-footer
+    >
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+        <b-form-group
+          id="form-title-group"
+          label="Name:"
+          label-for="form-title-input"
+        >
+          <b-form-input
+            id="form-title-input"
+            type="text"
+            v-model="editForm.name"
+            required
+            placeholder="Enter name"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="form-location-group"
+          label="Location:"
+          label-for="form-location-input"
+        >
+          <b-form-input
+            id="form-location-input"
+            type="text"
+            v-model="editForm.location"
+            required
+            placeholder="Enter location"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="form-date-group"
+          label="Date:"
+          label-for="form-date-input"
+        >
+          <b-form-input
+            id="form-date-input"
+            type="text"
+            v-model="editForm.date"
+            required
+            placeholder="Enter date"
+          >
+          </b-form-input>
+          <b-form-group
+            id="form-time-group"
+            label="Time:"
+            label-for="form-time-input"
+          >
+            <b-form-input
+              id="form-time-input"
+              type="text"
+              v-model="editForm.time"
+              required
+              placeholder="Enter time"
+            >
+            </b-form-input>
+          </b-form-group>
+        </b-form-group>
+        <b-button type="submit" class="mr-2" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Alert from '../Alert.vue';
 
 export default {
   name: 'Home',
+  components: {
+    alert: Alert,
+  },
   data() {
     return {
       events: [],
+      addEventForm: {
+        name: '',
+        location: '',
+        date: '',
+        time: '',
+      },
+      editForm: {
+        name: '',
+        location: '',
+        date: '',
+        time: '',
+      },
+      message: '',
+      showMessage: false,
     };
   },
   methods: {
@@ -81,6 +244,111 @@ export default {
           console.error(error);
         });
     },
+    addEvent(payload) {
+      const path = 'http://localhost:5000/events';
+      axios
+        .post(path, payload)
+        .then(() => {
+          this.getEvents();
+          this.message = 'Event added!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.getEvents();
+        });
+    },
+    updateEvent(payload, eventID) {
+      const path = `http://localhost:5000/events/${eventID}`;
+      axios
+        .put(path, payload)
+        .then(() => {
+          this.getEvents();
+          this.message = 'Event updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getEvents();
+        });
+    },
+    removeEvent(eventID) {
+      const path = `http://localhost:5000/events/${eventID}`;
+      axios.delete(path)
+        .then(() => {
+          this.getEvents();
+          this.message = 'Event removed!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          console.error(error);
+          this.getEvents();
+        });
+    },
+    onDeleteEvent(event) {
+      this.removeEvent(event.id);
+    },
+
+    editEvent(event) {
+      this.editForm = event;
+    },
+    initForm() {
+      this.addEventForm.name = '';
+      this.addEventForm.location = '';
+      this.addEventForm.date = '';
+      this.addEventForm.time = '';
+      this.editForm.name = '';
+      this.editForm.location = '';
+      this.editForm.date = '';
+      this.editForm.time = '';
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addEventModal.hide();
+      const payload = {
+        name: this.addEventForm.name,
+        location: this.addEventForm.location,
+        date: this.addEventForm.date,
+        time: this.addEventForm.time,
+      };
+      this.addEvent(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addEventModal.hide();
+      this.initForm();
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editEventModal.hide();
+      const payload = {
+        name: this.editForm.name,
+        location: this.editForm.location,
+        date: this.editForm.date,
+        time: this.editForm.time,
+
+      };
+      this.updateEvent(payload, this.editForm.id);
+    },
+    onUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editEventModal.hide();
+      const payload = {
+        name: this.editForm.name,
+        location: this.editForm.location,
+        date: this.editForm.date,
+        time: this.editForm.time,
+      };
+      this.updateEvent(payload, this.editForm.id);
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editEventModal.hide();
+      this.initForm();
+      this.getEvents();
+    },
   },
   created() {
     this.getEvents();
@@ -95,21 +363,27 @@ export default {
   z-index: 1;
   top: 0;
   left: 0;
-  background-color: #111;
+  background-color: #33313b;
   overflow-x: hidden;
   padding-top: 20px;
+  border-right: 2px solid #f5f5f5;
 }
 
 .sidenav a {
-  padding: 6px 8px 6px 16px;
+  padding: 16px;
   text-decoration: none;
   font-size: 25px;
   color: #818181;
   display: block;
+  text-transform: uppercase;
 }
-
 .sidenav a:hover {
   color: #f1f1f1;
+  background-color: #818181;
+  border-radius: 50px;
+}
+.links {
+  margin-top: 40px;
 }
 
 .main {
@@ -117,7 +391,7 @@ export default {
   padding: 0px 10px;
 }
 .card-outer {
-  /* Add shadows to create the "card" effect */
+  /* Add shadows to create the 'card' effect */
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   border-radius: 10px;
   transition: 0.3s;
@@ -138,17 +412,21 @@ export default {
   background-color: #f2b920;
   color: #fff;
 }
+.btn {
+  border-radius: 8px;
+  color: #fff;
+}
 .center {
   padding-bottom: 20px;
   justify-content: space-between;
   width: 900px;
   margin: 0 auto;
   padding: 15px;
-  background-color: #F0F2F5;
+  background-color: #f0f2f5;
 }
 .centered {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-    width: 900px;
-    margin: 0 auto
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  width: 900px;
+  margin: 0 auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 Vue.config.productionTip = false;
 
 new Vue({
